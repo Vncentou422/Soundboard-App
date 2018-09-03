575bd6d046c38387f90f9f38b2effe0ac67fe2d2
Vincent Ou <vncentou422@gmail.com>
2018-09-03 01:58:12 -0400

    adding button.png and beginning of recycler adapter

---
+++ Soundboard/app/src/main/java/com/dev/vincentou/soundboard/SoundboardRecyclerAdapter.java
@@ -0,0 +1,13 @@
+package com.dev.vincentou.soundboard;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+
+public class SoundboardRecyclerAdapter extends RecyclerView<SoundboardViewHolder>{
+
+
+    public SoundboardRecyclerAdapter(@NonNull Context context) {
+        super(context);
+    }
+}
